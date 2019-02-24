64b9d14aa93f2500f678fad8736da2d2f925f4b1
dakott@pobox.com <dakott@pobox.com>
2019-02-24 17:19:22 -0500

    Updated autoassistAlignment to allow tuning of drive PID, handle intermittent target information from vision processor, enforce coherent data from vision coprocessor (so targetHeading and Age are always from the same image.)

--- src/main/java/frc/robot/commands/autoassistAlignment.java
+++ src/main/java/frc/robot/commands/autoassistAlignment.java
@@ -26,18 +26,24 @@ public class autoassistAlignment extends Command {
   private gyroPIDSource m_gyroTurning = new gyroPIDSource();
   private double m_output;
   private pidOutput m_pidOutput = new pidOutput();
-  private NetworkTableEntry targetErrorEntry;
-  private NetworkTableEntry targetProcessingTimeEntry;
+  private NetworkTableEntry targetInformation;
+  private NetworkTableEntry autoAssistPID_Kp;
+  private NetworkTableEntry autoAssistPID_Ki;
+  private NetworkTableEntry autoAssistPID_Kd;
+  private double m_targetHeading;
+  private long m_targetHeadingTimestamp;
 
   private class gyroPIDSource implements PIDSource {
     @Override
     public double pidGet() {
-      return Robot.m_gyro.getGyroHeading();
+      return getCurrentHeading();
     }
+
     @Override
     public PIDSourceType getPIDSourceType() {
       return PIDSourceType.kDisplacement;
     }
+
     @Override
     public void setPIDSourceType(PIDSourceType pidSource) {
       /* don't care about this */
@@ -55,16 +61,116 @@ public class autoassistAlignment extends Command {
     requires(Robot.m_gyro);
     requires(Robot.m_drive);
 
-    m_PidControllerLeftRight = new PIDController(0.2, 0, 0, m_gyroTurning, m_pidOutput);
+    m_PidControllerLeftRight = new PIDController(0.2, 0, 0, m_gyroTurning, m_pidOutput, 0.02);
     NetworkTableInstance ntinst = NetworkTableInstance.getDefault();
     NetworkTable visionTable = ntinst.getTable("Vision");
-    targetErrorEntry = visionTable.getEntry("targetError");
-    targetProcessingTimeEntry = visionTable.getEntry("targetProcessingTime");
+    targetInformation = visionTable.getEntry("targetInformation");
 
-    targetProcessingTimeEntry.addListener(event -> {
-      double targetHeading = getTargetHeading();
-      m_PidControllerLeftRight.setSetpoint(targetHeading);
-      SmartDashboard.putNumber("autoAssistTargetHeading", targetHeading);
+    NetworkTable smartDashNetworkTable = NetworkTableInstance.getDefault().getTable("SmartDashboard");
+
+    /*
+     * Create some controls for the PID controlling the autoassist drive so we can
+     * tune them on a real robot.
+     */
+    autoAssistPID_Kp = smartDashNetworkTable.getEntry("autoAssistPID_Kp");
+    autoAssistPID_Ki = smartDashNetworkTable.getEntry("autoAssistPID_Ki");
+    autoAssistPID_Kd = smartDashNetworkTable.getEntry("autoAssistPID_Kd");
+
+    /*
+     * Initialize the dashboard controls with the current configuration from the PID
+     * so they have realistic values.
+     */
+    autoAssistPID_Kp.setDouble(m_PidControllerLeftRight.getP());
+    autoAssistPID_Ki.setDouble(m_PidControllerLeftRight.getI());
+    autoAssistPID_Kd.setDouble(m_PidControllerLeftRight.getD());
+
+    /*
+     * When the autoAssistPID_Kp value changes, update the PID with the new value
+     */
+    autoAssistPID_Kp.addListener(event -> {
+      m_PidControllerLeftRight.setP(autoAssistPID_Kp.getDouble(0.0));
+    }, EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
+
+    /*
+     * When the autoAssistPID_Ki value changes, update the PID with the new value
+     */
+    autoAssistPID_Ki.addListener(event -> {
+      m_PidControllerLeftRight.setI(autoAssistPID_Ki.getDouble(0.0));
+    }, EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
+
+    /*
+     * When the autoAssistPID_Kd value changes, update the PID with the new value
+     */
+    autoAssistPID_Kd.addListener(event -> {
+      m_PidControllerLeftRight.setD(autoAssistPID_Kd.getDouble(0.0));
+    }, EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
+
+    /*
+     * When new targetInformation data arrives from the coprocessor, compute the new
+     * heading and set the PID's setpoint to this new heading so the robot will
+     * start steering toward the vision target.
+     */
+    targetInformation.addListener(event -> {
+
+      try {
+        /*
+         * Extract the two numbers from the targetInformation string. For example, we'll
+         * get a string that looks like this:
+         * 
+         * 3.141568,150
+         * 
+         * Where 3.141568 is the heading of the target that the vision system found and
+         * 150 is the time the vision system took to process that image; it's the age of
+         * that target heading. Use the String.split() function to split the string from
+         * the coprocessor into two different strings right at the ','. The ',' is not
+         * included in either string; it's discarded by split(). Save the two strings
+         * into the array of strings strNumbers[]. The first number that was detected,
+         * the heading, is now in strNumbers[0], for example "3.141568". The second
+         * number that was detected, the age, is now in strNumbers[1], for example
+         * "150". These are still strings, not real numbers yet.
+         * 
+         * strNumbers[0] = "3.141568"
+         * 
+         * strNumbers[1] = "150"
+         */
+        String[] strNumbers = targetInformation.getString("").split(",");
+
+        /*
+         * Convert the strings into real numbers useful for math using the parseDouble()
+         * and parseLong() functions. (Double and Long types are special classes that
+         * mimic the simpler behavior of double and long, but also have helpful methods
+         * like parseDouble() and parseLong() to convert strings to real numbers.)
+         */
+        double targetHeadingAtTimestamp = Double.parseDouble(strNumbers[0]);
+        long targetHeadingAge = Long.parseLong(strNumbers[1]);
+
+        /*
+         * Determine the timestamp, in terms of the roborio's clock, of the
+         * targetHeading received from the vision processor. Just subtract the age of
+         * the targetHeading from the roborio's timestamp (last_change) that represents
+         * when the message was received from the vision coprocessor. This should be
+         * very close to when the vision coprocessor started working on the image that
+         * it used to determine the heading of the target.
+         */
+        m_targetHeadingTimestamp = targetInformation.getInfo().last_change - targetHeadingAge;
+
+        /*
+         * Now look back in time to the heading of the robot (its "pose") at the time
+         * the image was taken by the vision coprocessor. Then, simply determine the
+         * targetHeading the coprocessor computed + the heading of the robot when it
+         * took the picture to figure out the new heading of the robot.
+         */
+        m_targetHeading = getPreviousRobotHeading(m_targetHeadingTimestamp) + targetHeadingAtTimestamp;
+
+        /* Tell the PID that's steering the robot where its new heading is. */
+        m_PidControllerLeftRight.setSetpoint(m_targetHeading);
+        SmartDashboard.putNumber("autoAssistTargetHeading", m_targetHeading);
+
+      } catch (Exception e) {
+        System.out.println(String.format("Couldn't parse \"%s\" from vision coprocessor %s",
+            targetInformation.getString(""), e.toString()));
+      }
+
     }, EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
 
   }
@@ -90,22 +196,6 @@ public class autoassistAlignment extends Command {
 
     SmartDashboard.putNumber("autoAssistCurrentHeading", getCurrentHeading());
 
-  }
-
-  private double getTargetHeading() {
-
-    double Gyroheading = getCurrentHeading();
-
-    /*
-     * Compute the timestamp, in terms of the roborio's timebase, when the raspberry
-     * pi's vision target was computed.
-     */
-    long messageTimeStamp = (long) (targetProcessingTimeEntry.getInfo().last_change
-        - targetProcessingTimeEntry.getDouble(0));
-
-    double targetheading = getPreviousRobotHeading(messageTimeStamp) + targetErrorEntry.getDouble(Gyroheading);
-
-    return targetheading;
   }
 
   private double getCurrentHeading() {
@@ -135,7 +225,7 @@ public class autoassistAlignment extends Command {
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
-
+    
     m_PidControllerLeftRight.disable();
   }
 }
