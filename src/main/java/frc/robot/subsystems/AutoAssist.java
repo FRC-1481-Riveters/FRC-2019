c469e945c14864f8f2069ca24638dfd47bc64f10
dakott@pobox.com <dakott@pobox.com>
2019-03-17 18:20:30 -0400

    Cleared sticky faults in PCM supporting AutoAssist camera light to support more robust performance during intermittent shorts. Added better documentation to make the subystem's purpose more clear.

--- src/main/java/frc/robot/subsystems/AutoAssist.java
+++ src/main/java/frc/robot/subsystems/AutoAssist.java
@@ -18,16 +18,44 @@ public class AutoAssist extends Subsystem {
 
   public void disabledInit() {
 
+    /*
+     * Turn off the camera light any time the module enters disabled mode because we
+     * don't need the light when we're not capable of moving.
+     */
     m_autoassistCameraLightControl.set(false);
   }
 
   public void autonomousInit() {
 
+    /*
+     * Turn on the camera light any time the module enters auto mode because we need
+     * the light when we're capable of moving and using autoassist.
+     * 
+     * If there's any sticky faults due to e.g. an intermittent short on the PCM
+     * solenoid outputs, attempt to clear it before enabling this solenoid.
+     * 
+     * If the short circuit doesn't clear, the PCM will log a sticky fault again
+     * against this output channel and blacklist it (keep it from turning on to keep
+     * the rest of the non-shorted PCM's outputs working.)
+     */
+    m_autoassistCameraLightControl.clearAllPCMStickyFaults();
     m_autoassistCameraLightControl.set(true);
   }
 
   public void teleopInit() {
-    
+
+    /*
+     * Turn on the camera light any time the module enters teleop mode because we
+     * need the light when we're capable of moving and using autoassist.
+     * 
+     * If there's any sticky faults due to e.g. an intermittent short on the PCM
+     * solenoid outputs, attempt to clear it before enabling this solenoid.
+     * 
+     * If the short circuit doesn't clear, the PCM will log a sticky fault again
+     * against this output channel and blacklist it (keep it from turning on to keep
+     * the rest of the non-shorted PCM's outputs working.)
+     */
+    m_autoassistCameraLightControl.clearAllPCMStickyFaults();
     m_autoassistCameraLightControl.set(true);
   }
 
