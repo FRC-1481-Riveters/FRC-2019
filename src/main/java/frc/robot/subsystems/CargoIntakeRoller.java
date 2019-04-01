50116a056994d8d67d87efa52785f6642811cdee
dakott@pobox.com <dakott@pobox.com>
2019-03-31 23:23:34 -0400

    Improved IRSensor code to comprehend some sensors we use. Updated CargoIntakeRoller to use new sensor interfaces.

--- src/main/java/frc/robot/subsystems/CargoIntakeRoller.java
+++ src/main/java/frc/robot/subsystems/CargoIntakeRoller.java
@@ -22,18 +22,19 @@ public class CargoIntakeRoller extends Subsystem {
   // here. Call these from Commands.
   public static WPI_TalonSRX m_cargo_arm_roller_talon = new WPI_TalonSRX(RobotMap.cargoIntakeArm_Talon);
 
-  private IRSensor m_irSensor = new IRSensor(0);
+  private IRSensor m_irSensor = new IRSensor(0, IRSensor.sensorType.GP2Y0A51SK0F);
 
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
   }
+
   public void setSpeed(double IntakeRollerSpeed) {
     m_cargo_arm_roller_talon.set(ControlMode.PercentOutput, IntakeRollerSpeed);
   }
 
   public boolean isCargoDetected() {
     return m_irSensor.getRawVoltage() < RobotMap.intakeSensorBallPresent;
-   }
+  }
 }
--- src/main/java/irsensor/IRSensor.java
+++ src/main/java/irsensor/IRSensor.java
@@ -5,26 +5,54 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDSourceType;
+import java.util.HashMap;
+import java.util.Map;
 
 public class IRSensor extends SendableBase implements PIDSource {
 
     private AnalogInput m_anaInput;
-    private sensorType m_type;
+
+    protected class LinearizationConstants {
+        public double m;
+        public double b;
+        public double k;
+
+        public LinearizationConstants(double m, double b, double k) {
+            this.m = m;
+            this.b = b;
+            this.k = k;
+        }
+    }
+
+    private LinearizationConstants m_constants;
 
     public enum sensorType {
-        GP2Y0A51SK0F
+        GP2Y0A51SK0F, GP2Y0A41SK0F
     }
 
-    public IRSensor(int analogChannel) {
+    private Map<sensorType, LinearizationConstants> m_sensorData = new HashMap<>() {{
+        put(sensorType.GP2Y0A51SK0F, new LinearizationConstants(6.33, -0.474, 0.3));
+        put(sensorType.GP2Y0A41SK0F, new LinearizationConstants(6.33, -0.474, 0.3));
+    }};
+  
 
-        m_type = sensorType.GP2Y0A51SK0F;
-        m_anaInput = new AnalogInput(analogChannel);
+    private LinearizationConstants getConstant(sensorType type) {
+        LinearizationConstants constantValues;
+        if (m_sensorData.containsKey(type)) {
+            constantValues = m_sensorData.get(type);
+        } else {
+            constantValues = m_sensorData.get(sensorType.GP2Y0A51SK0F);
+        }
+
+        return constantValues;
     }
+
 
     public IRSensor(int analogChannel, sensorType type) {
 
-        m_type = type;
+        m_constants = getConstant(type);
         m_anaInput = new AnalogInput(analogChannel);
+        setName("IRSensor_" + type.toString(), analogChannel);
     }
 
     public double getRawVoltage() {
@@ -58,27 +86,30 @@ public class IRSensor extends SendableBase implements PIDSource {
      * @return the result to use in PIDController
      */
     public double pidGet() {
-        return getRangeInches();
+        return getRangeCm();
     }
 
     public double getRawCounts() {
         return m_anaInput.getValue();
     }
 
-    @Deprecated
-    public void setSensorType(sensorType type) {
-        m_type = type;
-    }
+    public double getRangeCm() {
+        double voltage = getRawVoltage();
 
-    @Deprecated
-    public double getRangeInches() {
+        try {
+            return (1.0 / ((m_constants.m * voltage) + m_constants.b)) - m_constants.k;
+        } catch (ArithmeticException ex) {
+            System.out.printf("IRSensor couldn't compute range from voltage %3.2f. Returned 0.0: %s", voltage,
+                    ex.toString());
+        }
+
         return 0.0;
     }
 
     @Override
     public void initSendable(SendableBuilder builder) {
         builder.setSmartDashboardType("IRSensor");
-        builder.addDoubleProperty("Value", this::getRangeInches, null);
+        builder.addDoubleProperty("Range (cm)", this::getRangeCm, null);
         builder.addDoubleProperty("RawVolts", this::getRawVoltage, null);
         builder.addDoubleProperty("RawCounts", this::getRawCounts, null);
     }
